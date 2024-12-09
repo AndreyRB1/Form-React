f3a81debf3c41b29191784b6999000d08b5de928
AndreyRB1 <darb.styl3@gmail.com>
2024-12-09 16:32:40 -0500

    añadida la imagen del control con el efecto de zoom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import styled from "styled-components";
-import { Form , Container_Form, Section, Figure, TeamImput, Input} from "./elements/Form"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircleXmark } from '@fortawesome/free-solid-svg-icons'
-import { faCircleCheck } from '@fortawesome/free-solid-svg-icons'
+import Imagenes from "./assets/imagenes";
+import { Form , Container_Form, Section, Figure, TeamImput, Input, InputError, ValidationIcon} from "./elements/Form"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
+import { faCircleCheck } from '@fortawesome/free-solid-svg-icons';
+import { faCircleExclamation} from '@fortawesome/free-solid-svg-icons';
+
 
 
 
@@ -13,15 +16,24 @@ const app = () => {
       <Section>
         <Container_Form>
           <Form action="">
+            <TeamImput>
+            <Input type="text" placeholder="CORREO" id="correo"></Input>
+            <ValidationIcon icon={faCircleXmark} />
+            </TeamImput>
+            <InputError>lorem as</InputError>
             <div>
-            <input type="text" placeholder="CORREO"></input>
-            <FontAwesomeIcon icon={faCircleXmark} />
+              <p>
+                <FontAwesomeIcon icon={faCircleExclamation}/>
+                <b>ERROR</b> POR FAVOR RELLENAR EL FORMULARIO CORRECTAMENTE
+              </p>
             </div>
-            <p>lorem asd</p>
+            <div>
+              <button type="submit">Enviar</button>
+             </div>
           </Form>
         </Container_Form>
         <Figure>
-        <img />
+        <img src={Imagenes[0]}/>
         </Figure>
       </Section>
     </main>
--- src/elements/Form.js
+++ src/elements/Form.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const colors = {
     border: "#0075FF",
@@ -13,44 +14,106 @@ const Section = styled.form`
     flexDirection: row;
 `;
 
+const Container_Form = styled.form`
+    background: purple;
+    width: 50%;
+    height: 100vh;
+
+
+`;
+
 const Form = styled.form`
-    background: yellow ;
+    width: 100%;
+    height: 100vh;
+    background: pink ;
     padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center:
 
 `;
 
 const TeamImput = styled.div`
     position: 'relative';
     zIndex: 90;
+    background: yellow;
+    align-items:center;
+    justify-content:center;
+    display: flex;  
 `;
 
 const Input = styled.input`
-    background-color: #522d9f;
-    width: 80%;
+    background: #522d9f;
+    width: 50%;
     height: 40px;
     border-radius:7px;
     outline: none;
     border: none;
-    padding-left: 10px;
+    padding: 10px 40px;
+    transition: .3s ease all;    
     color: aliceblue;
     margin-bottom: 5px;
+    border: 3px solid transparent;
+
+    &:focus {
+        border: 3px solid ${colors.border};
+        outline: none;
+        box-shadow: 3px 0px 30px rgba(163, 163 ,163, 0.4);
+    }
     
 `;
 
-const Container_Form = styled.form`
-    background: purple;
-    width: 50%;
-    height: 100vh;
-
+const InputError = styled.p`
+    font-size: 12px;
+    margin-bottom: 0;
+    color: ${colors.error};
 `;
+
+const ValidationIcon = styled(FontAwesomeIcon) `
+    position: absolute;
+    right: 10px;
+    bottom: 14px;
+    zIndex: 100;
+    font-size: 16px;
+    opacity: 0;
+`;
+
 
 const Figure = styled.form`
-    background: yellow;
+    background-image: linear-gradient(
+        135deg,
+        hsl(0deg 0% 0%) 0%,
+        hsl(257deg 24% 12%) 29%,
+        hsl(257deg 52% 14%) 47%,
+        hsl(257deg 61% 14%) 65%,
+        hsl(256deg 29% 13%) 100%
+      );
+    padding-top: 40px;
     width: 50%;
     height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    box-sizing: border-box;
+
+    img {
+    transition: 0.2s;
+    width: 50%;
+    max-width: 100%; /* Previene que la imagen exceda el tamaño del contenedor */
+    height: auto;
+    }
+
+    img:hover {
+        width: 80%;
+    }
+
 `;
 
 
 
 
-export {Form, Container_Form, Section, Figure, TeamImput, Input};
+export {Form, Container_Form, Section, Figure, TeamImput, Input,
+        InputError, ValidationIcon
+        
+};
